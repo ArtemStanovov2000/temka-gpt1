7c7aac4ea1fdc1e3a30e7bb430cff6eac6bccfdb
Artem <106746318+ArtemStanovov2000@users.noreply.github.com>
2025-05-18 22:34:28 +0300

    Сделал экспериментальные воркеры для работы внимания

--- src/data/attention/selfAttention.ts
+++ src/data/attention/selfAttention.ts
@@ -1,13 +1,28 @@
 import { layerNorm } from "./layerNorn"
 import { calculateOneHeadW_QKV_Matrix } from "./calculateQueryMatrix"
+import { embeddingMatrix } from "../matrix/embeddingMatrix"
+import { W_Q_1layer } from "../matrix/layer_1/W_Q_1layer"
+import { W_K_1layer } from "../matrix/layer_1/W_K_1layer"
+import { W_V_1layer } from "../matrix/layer_1/W_V_1layer"
 
 export const selfAttention = (emb?: number[][], matrixQ?: number[][], matrixK?: number[][], matrixV?: number[][], gamma?: number[], beta?: number[]) => {
-    //const normEmb = layerNorm(emb, gamma, beta)
+    //const normEmb = layerNorm(emb, gamma, beta) /
 
-    const worker = new Worker(new URL("./worker.ts", import.meta.url))
-    worker.postMessage({ message: '415-ый, я база, ответьте' })
+    const headAttention1Query = new Worker(new URL("./worker.ts", import.meta.url))
+    const headAttention1Key = new Worker(new URL("./worker.ts", import.meta.url))
+    const headAttention1Value = new Worker(new URL("./worker.ts", import.meta.url))
 
-    worker.onmessage = function (e) {
+    headAttention1Query.postMessage({ embeddings: embeddingMatrix, matrix: W_Q_1layer, start: 0, end: 8 })
+    headAttention1Key.postMessage({ embeddings: embeddingMatrix, matrix: W_K_1layer, start: 0, end: 8 })
+    headAttention1Value.postMessage({ embeddings: embeddingMatrix, matrix: W_V_1layer, start: 0, end: 8 })
+
+    headAttention1Query.onmessage = function (e) {
+        console.log(e.data)
+    }
+    headAttention1Key.onmessage = function (e) {
+        console.log(e.data)
+    }
+    headAttention1Value.onmessage = function (e) {
         console.log(e.data)
     }
 }
--- src/data/attention/worker.ts
+++ src/data/attention/worker.ts
@@ -1,6 +1,19 @@
-onmessage = function (e) { // Слушаем сообщения из основного потока
-  if (e.data.message === '415-ый, я база, ответьте') {
-    // Отправляем сообщение из воркера в основной поток
-    postMessage('База, это 415-ый, как слышно?')
-  }
+onmessage = function (e) {
+    const headMatrix: number[][] = []
+    for (let i = e.data.start; i < e.data.end; i++) {
+        headMatrix.push(e.data.matrix[i])
+    }
+
+    const result: number[][] = [];
+    for (let i = 0; i < 512; i++) {
+        result[i] = [];
+        for (let j = 0; j < headMatrix.length; j++) {
+            let sum = 0;
+            for (let k = 0; k < e.data.embeddings[0].length; k++) {
+                sum += e.data.embeddings[i][k] * headMatrix[j][k];
+            }
+            result[i][j] = sum;
+        }
+    }
+    postMessage(result)
 }
