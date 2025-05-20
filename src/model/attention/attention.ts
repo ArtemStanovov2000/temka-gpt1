30851cceb7259c7980af0375079168a7e658f258
Artem <106746318+ArtemStanovov2000@users.noreply.github.com>
2025-05-20 20:27:44 +0300

    Рассчитал матрицу внимания синхронно

--- src/model/attention/attention.ts
+++ src/model/attention/attention.ts
@@ -1,9 +1,17 @@
-export const attention = (embeddings: number[][]) => {
-    // Нормализация эмбеддингов
-    // умножение на матрицу W_q
-    // умножение на матрицу W_k
-    // умножение на матрицу W_v
+import { layerNorm } from "../../utils/mathematicalOperations/layerNorn"
+import { calculateAttentionMatrix } from "./calculateAttentionMatrix"
+import { calculateOneHeadW_QKV_Matrix } from "./calculateQueryMatrix"
 
+export const attention = (embeddings: number[][], gammaFirst: number[], betaFirst: number[], gammaSecond: number[], betaSecond: number[], W_q: number[][], W_k: number[][], W_v: number[][]) => {
+    const embeddingsNorm = layerNorm(embeddings, gammaFirst, betaFirst)
+
+    const W_QMatrix = calculateOneHeadW_QKV_Matrix(embeddingsNorm, W_q, 0, 128)
+    const W_KMatrix = calculateOneHeadW_QKV_Matrix(embeddingsNorm, W_k, 0, 128)
+    const W_VMatrix = calculateOneHeadW_QKV_Matrix(embeddingsNorm, W_v, 0, 128)
+
+    // Применить маскирование
+    const attentionMatrix = calculateAttentionMatrix(W_QMatrix, W_KMatrix)
+    console.log(attentionMatrix)
     // Вычисляем внимание умножая матрицу W_q на W_k
     // Умножаем на матрицу W_v возвращаем исходную размерность
 
--- src/model/createToken.ts
+++ src/model/createToken.ts
@@ -1,5 +1,14 @@
 import { textPreparation } from "./textPreparation/textPreparation"
+import { attention } from "./attention/attention"
+import { gamma1layerFirst } from "../data/matrix/layer_1/gamma1layerFirst"
+import { beta1layerFirst } from "../data/matrix/layer_1/beta1layerSecond"
+import { gamma1layerSecond } from "../data/matrix/layer_1/gamma1layerSecond"
+import { beta1layerSecond } from "../data/matrix/layer_1/beta1layerFirst"
+import { W_Q_1layer } from "../data/matrix/layer_1/W_Q_1layer"
+import { W_K_1layer } from "../data/matrix/layer_1/W_K_1layer"
+import { W_V_1layer } from "../data/matrix/layer_1/W_V_1layer"
 
 export const createToken = (text: string) => {
-    return textPreparation(text)
+    const initialEmb = textPreparation(text)
+    attention(initialEmb, gamma1layerFirst, beta1layerFirst, gamma1layerSecond, beta1layerSecond, W_Q_1layer, W_K_1layer, W_V_1layer)
 }
